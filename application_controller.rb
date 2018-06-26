053a8f46bc39b5a3580ea5b983c48b1b691cfad7
jli063 <jli063@rsdmo.org>
2018-06-26 16:53:18 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -3,8 +3,14 @@ Bundler.require
 
 class MyApp < Sinatra::Base  #
 
-  get '/' do
+  get '/' do #when we go to the homepage, it will be the index
     erb :index  #go to an embedded ruby file called index
   end
+  
+  get '/IHopeThisWorks' do
+    erb :page2
+  end
+  
+  
 
 end
